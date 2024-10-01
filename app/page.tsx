170afbaef47f661e100a499eafebeb1ebeec3174
Josh Steiner <josh@vitriolix.com>
2024-09-30 20:59:08 -0700

    get rid of colon

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
               Haters
             </i>
           </b>
-        </a>:
+        </a>
         </p>
         <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay"
                 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1926365390&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
